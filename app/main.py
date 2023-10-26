193b6442a9bc2d8136e72d2885487f9480d1098f
Vikkktorrria <kopylskikh.vm@mail.ru>
2023-10-26 14:44:11 +0500

    модель сообщений для системы мониторинга

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import psycopg2
 from db import consts as db_consts
+import datetime
+import requests
 
 app = FastAPI()
 
@@ -11,20 +13,56 @@ pg_connection = psycopg2.connect(
     password=db_consts.DB_PASSWORD)
 
 
+def in_monitoring_system(in_date_time, f_name):
+    print('in запрос отправлен', in_date_time, f_name)
+
+    url = "http://monitoring-service-url"  # допустим, настоящий url
+    message = {
+        "date_time": in_date_time,
+        "function": f_name,
+    }
+    response = requests.post(url, json=message)
+    return response.status_code
+
+
+def out_monitoring_system(out_date_time, f_name, status):
+    print('out запрос отправлен', out_date_time, f_name, status)
+
+    url = "http://monitoring-service-url"  # допустим, настоящий url
+    message = {
+        "date_time": out_date_time,
+        "function": f_name,
+        "status": status
+    }
+    response = requests.post(url, json=message)
+    return response.status_code
+
+
+def send_message(func):
+    def wrapper(*args, **kwargs):
+        in_monitoring_system(datetime.datetime.now(), func.__name__)
+        result = func()
+        out_monitoring_system(datetime.datetime.now(), func.__name__, 200)
+        return result
+    return wrapper
+
+
+@send_message
 @app.post("/get_layer_objects/")
 def get_layer_objects(layer_id: int):
     """
-    Функция возвращает объекто выбранного слоя
+    Функция возвращает объект выбранного слоя
     :return:
     """
     # Open a cursor to perform database operations
-    # print(request.data)
     cur = pg_connection.cursor()
     query = """
-            SELECT geometry_object.id, data 
-            FROM geometry_object JOIN layer 
+            SELECT geometry_object.id, data
+            FROM geometry_object JOIN layer
             ON geometry_object.id=layer.id;
     """
     cur.execute(query)
     res = cur.fetchall()
     return res
+
+
